0ef6efc65a79352f08c8f3dd3b28db5b83358400
ThijsBrekhof <thijs.brekhof@gmail.com>
2022-05-03 15:28:21 +0200

    Changed calculate_iaa.py so it can work with un-annotated comments.

--- Data/Annotation data and scripts/calculate_iaa.py
+++ Data/Annotation data and scripts/calculate_iaa.py
@@ -1,6 +1,12 @@
+# Author: Thijs Brekhof
+
 import json
-import sklearn
+
 from sklearn.metrics import cohen_kappa_score
+
+
+def get_id(data):
+    return data.get('id')
 
 
 def main():
@@ -12,17 +18,31 @@ def main():
     with open('pilot_annot_thijs.json', encoding='utf8') as json_file2:
         data2 = json.load(json_file2)
 
+    # sorting the data (by default the order is weird for some reason)
+    data1.sort(key=get_id)
+    data2.sort(key=get_id)
+
     # Getting annotations from annotator 1
     label_list_1 = []
-    for count, line in enumerate(data1):
-        label_list_1.append(line["annotations"][0]["result"][0]["value"]['choices'][0])
+    for i in range(1, 478):
+
+        if i == data1[0].get('id'):
+            label_list_1.append(data1[0]["annotations"][0]["result"][0]["value"]['choices'][0])
+            del data1[0]
+        else:
+            label_list_1.append("multiple")
 
     # Getting annotations from annotator 2
     label_list_2 = []
-    for count, line in enumerate(data2):
-        label_list_2.append(line["annotations"][0]["result"][0]["value"]['choices'][0])
+    for i in range(1, 478):
 
-    # Calculating IAA
+        if i == data2[0].get('id'):
+            label_list_2.append(data2[0]["annotations"][0]["result"][0]["value"]['choices'][0])
+            del data2[0]
+        else:
+            label_list_2.append("multiple")
+
+    #  Calculating IAA
     print(cohen_kappa_score(label_list_1, label_list_2))
 
 
